8ac2b5d9fde4101ba388eb0ee6f5a813873a4091
Adriano Gonçalves de Souza <adriangonc@yahoo.com.br>
2021-04-28 09:44:56 -0300

    First commit

---
+++ introducao/basic-example-01.py
@@ -0,0 +1,26 @@
+number = 2
+
+vector = [2, 3, 5]
+
+import numpy as np
+vector = np.asarray(vector)
+
+vector.max()
+vector.argmax()
+vector.min()
+vector.mean()
+vector.sum()
+
+vector2 = number * vector
+
+np.arange(1, 50, 0.5)
+
+np.zeros(1532)
+
+np.zeros((3,3))
+
+np.linspace(1, 20, 10)
+
+np.random.randint(0, 100)
+
+print(np.random.randint(0, 101, (3,3)) )
